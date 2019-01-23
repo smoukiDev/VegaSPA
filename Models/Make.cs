d5bef940cf12aabeb93eb40352258dfdda095b10
Serhii Maksimchuk <smakdealcase@gmail.com>
2019-01-23 17:01:34 +0200

    Added domain model for Make and Model.

---
+++ Models/Make.cs
@@ -0,0 +1,16 @@
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+
+namespace VegaSPA.Models
+{
+    public class Make
+    {
+        public Make()
+        {
+            Models = new Collection<Model>();
+        }
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public ICollection<Model> Models {get; set;}
+    }
+}
