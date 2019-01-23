64b7a84df57056a815fbefc26dceac59a35d15fa
Serhii Maksimchuk <smakdealcase@gmail.com>
2019-01-23 16:42:54 +0200

    Set Kestrel in Program.cs. Add EFCore packages.

--- Program.cs
+++ Program.cs
@@ -19,6 +19,9 @@ namespace VegaSPA
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
+                .UseKestrel()
+                .UseContentRoot(Directory.GetCurrentDirectory())
+                .UseIISIntegration()
                 .UseStartup<Startup>();
     }
 }
