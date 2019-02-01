d818776dd99e2e3cdd674b4ad2495f7c1e64f674
Serhii Maksymchuk <smakdealcase@gmail.com>
2019-02-01 12:23:19 +0200

    Added and applied migration AddVihicle.

---
+++ Migrations/20190201101435_AddVihicle.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace VegaSPA.Migrations
+{
+    public partial class AddVihicle : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Vehicles",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    ModelId = table.Column<int>(nullable: false),
+                    IsRegistered = table.Column<bool>(nullable: false),
+                    ContactInfo_ContactName = table.Column<string>(maxLength: 255, nullable: false),
+                    ContactInfo_ContactEmail = table.Column<string>(maxLength: 255, nullable: true),
+                    ContactInfo_ContactPhone = table.Column<string>(maxLength: 255, nullable: false),
+                    LastModified = table.Column<DateTime>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Vehicles", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Vehicles_Models_ModelId",
+                        column: x => x.ModelId,
+                        principalTable: "Models",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "VihicleFeatures",
+                columns: table => new
+                {
+                    VihicleId = table.Column<int>(nullable: false),
+                    FeatureId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_VihicleFeatures", x => new { x.VihicleId, x.FeatureId });
+                    table.ForeignKey(
+                        name: "FK_VihicleFeatures_Features_FeatureId",
+                        column: x => x.FeatureId,
+                        principalTable: "Features",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_VihicleFeatures_Vehicles_VihicleId",
+                        column: x => x.VihicleId,
+                        principalTable: "Vehicles",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Vehicles_ModelId",
+                table: "Vehicles",
+                column: "ModelId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_VihicleFeatures_FeatureId",
+                table: "VihicleFeatures",
+                column: "FeatureId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "VihicleFeatures");
+
+            migrationBuilder.DropTable(
+                name: "Vehicles");
+        }
+    }
+}
