8545cdd03d91a08d8152acc5d6c2f2f6500c46a4
Serhii Maksymchuk <smakdealcase@gmail.com>
2019-02-22 13:57:02 +0200

    Refactored vehicle-service, extracted baseUrls to variables.

--- ClientApp/src/services/vehicle.service.ts
+++ ClientApp/src/services/vehicle.service.ts
@@ -6,34 +6,37 @@ import { HttpClient } from '@angular/common/http';
   providedIn: 'root'
 })
 export class VehicleService {
+  private readonly vehiclesBaseUrl = '/api/vehicles';
+  private readonly makesBaseUrl = '/api/makes';
+  private readonly featuresBaseUrl = '/api/features';
 
   constructor(private http: HttpClient) { }
 
   getMakes() {
-    return this.http.get('/api/makes');
+    return this.http.get(this.makesBaseUrl);
   }
 
   getFeatures() {
-    return this.http.get('/api/features');
+    return this.http.get(this.featuresBaseUrl);
   }
 
   createVehicle(vehicle) {
-    return this.http.post('/api/vehicles', vehicle);
+    return this.http.post(this.vehiclesBaseUrl, vehicle);
   }
 
   getVehicle(id) {
-    return this.http.get('/api/vehicles/' + id);
+    return this.http.get(this.vehiclesBaseUrl + id);
   }
 
   updateVehicle(vehicle: SaveVehicle) {
-    return this.http.put('/api/vehicles/' + vehicle.id, vehicle);
+    return this.http.put(this.vehiclesBaseUrl + vehicle.id, vehicle);
   }
 
   deleteVehicle(id: number) {
-    return this.http.delete('/api/vehicles/' + id);
+    return this.http.delete(this.vehiclesBaseUrl + id);
   }
 
   getVehicles() {
-    return this.http.get('/api/vehicles/');
+    return this.http.get(this.vehiclesBaseUrl);
   }
 }
