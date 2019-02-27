ce01a7bb8dda9a8866bd9230c7fae62c97676f4e
Serhii Maksymchuk <smakdealcase@gmail.com>
2019-02-27 13:12:18 +0200

    Implemented base logic of pagination component.

--- ClientApp/src/app/shared/pagination/pagination.component.ts
+++ ClientApp/src/app/shared/pagination/pagination.component.ts
@@ -1,15 +1,63 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter, OnChanges } from '@angular/core';
 
 @Component({
   selector: 'app-pagination',
   templateUrl: './pagination.component.html',
   styleUrls: ['./pagination.component.css']
 })
-export class PaginationComponent implements OnInit {
+export class PaginationComponent implements OnInit, OnChanges {
+  @Input('total-items') totalItems;
+  @Input('page-size') pageSize;
+  @Output('page-changed') pageChanged = new EventEmitter();
+  pages: any[];
+  currentPage;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor() { 
   }
 
+  ngOnChanges(): void {
+    this.currentPage = 1;
+    this.CountPagesAmount();
+  }
+
+  ngOnInit() { }
+
+  onFirst() {
+    this.currentPage = 1;
+    this.pageChanged.emit(this.currentPage);
+  }
+
+  onPrevious() {
+    if (this.currentPage == 1) {
+      return;
+    }
+    this.currentPage--;
+    this.pageChanged.emit(this.currentPage);
+  }
+
+  onPage(page) {
+    this.currentPage = page;
+    this.pageChanged.emit(page);
+  }
+
+  onNext() {
+    if (this.currentPage == this.pages.length) {
+      return;
+    }
+    this.currentPage++;
+    this.pageChanged.emit(this.currentPage);
+  }
+
+  onLast() {
+    this.currentPage = this.pages.length;
+    this.pageChanged.emit(this.currentPage);
+  }
+
+  private CountPagesAmount() {
+    let pagesCount = Math.ceil(this.totalItems / this.pageSize);
+    this.pages = [];
+    for (let index = 1; index <= pagesCount; index++) {
+      this.pages.push(index);
+    }
+  }
 }
