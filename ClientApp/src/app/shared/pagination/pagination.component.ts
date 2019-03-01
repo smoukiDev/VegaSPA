16b6a330805eee21ba746acc10da9b7c9f858f52
Serhii Maksimchuk <smakdealcase@gmail.com>
2019-03-01 22:46:01 +0200

    Integrate pagination pages range limit.

--- ClientApp/src/app/shared/pagination/pagination.component.ts
+++ ClientApp/src/app/shared/pagination/pagination.component.ts
@@ -9,6 +9,7 @@ export class PaginationComponent implements OnInit, OnChanges {
   @Input('total-items') totalItems;
   @Input('page-size') pageSize;
   @Input('current-page') currentPage = 1;
+  @Input('pages-between') pagesBetween;
   @Output('page-changed') pageChanged = new EventEmitter();
   pages: any[] = [];
 
@@ -58,5 +59,28 @@ export class PaginationComponent implements OnInit, OnChanges {
     for (let index = 1; index <= pagesCount; index++) {
       this.pages.push(index);
     }
+  }
+
+  private getPagesRange() {
+    const c = Math.ceil(this.totalItems / this.pageSize);
+    const p = this.currentPage || 1;
+    const pagesToShow = this.pagesBetween || 3;
+    const pages: number[] = [];
+    pages.push(p);
+    const times = pagesToShow - 1;
+    for (let i = 0; i < times; i++) {
+      if (pages.length < pagesToShow) {
+        if (Math.min.apply(null, pages) > 1) {
+          pages.push(Math.min.apply(null, pages) - 1);
+        }
+      }
+      if (pages.length < pagesToShow) {
+        if (Math.max.apply(null, pages) < c) {
+          pages.push(Math.max.apply(null, pages) + 1);
+        }
+      }
+    }
+    pages.sort((a, b) => a - b);
+    return pages;
   }
 }
